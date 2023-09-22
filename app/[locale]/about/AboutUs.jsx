b2cf6509f9c893b10c6491b850095747a500ace4
dhruv8433 <dhruvrsoni04@gmail.com>
2023-09-22 10:47:49 +0530

    About Page

--- app/[locale]/about/AboutUs.jsx
+++ app/[locale]/about/AboutUs.jsx
@@ -29,14 +29,17 @@ const AboutUs = () => {
         </div>
       </Grid>
       <Grid item xs={12} md={6}>
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="min-h-screen flex items-center justify-center bg-theme">
           <section className="py-16">
             <div className="mx-auto flex justify-center flex-col items-center">
               <h2 className="text-3xl font-semibold text-center text-gray-800 mb-8">
                 About eRequirement
               </h2>
               {/* we use dangerouslySetInnerHTML to set html inside dynamic div and pass __html to set data there*/}
-              <div className="px-10" dangerouslySetInnerHTML={{ __html: aboutUs }}></div>
+              <div
+                className="px-10"
+                dangerouslySetInnerHTML={{ __html: aboutUs }}
+              ></div>
             </div>
           </section>
         </div>
