93ea65e25ca316dfa63c486e9698e135a32138db
dhruv8433 <dhruvrsoni04@gmail.com>
2023-09-22 19:40:59 +0530

    configuration

--- app/config/config.js
+++ app/config/config.js
@@ -10,6 +10,18 @@ export const colors = [
 
 export const company_name = `eRequirement`;
 
+// if provider id is inside this array than only return service regarding those provider
+export const providersId = [21, 22, 23, 24, 25, 26, 27, 28, 29];
+export const homeId = 21;
+export const laundryId = 22;
+export const fullServiceId = 23;
+export const newBarberId = 24;
+export const electricId = 25;
+export const pestControlId = 26;
+export const plumbingId = 27;
+export const kitchenExprertId = 28;
+export const carProviderId = 29;
+
 // function that help us to open and close anything using boolean values
 export function open(open) {
   open(true);
