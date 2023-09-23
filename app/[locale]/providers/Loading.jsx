f052e41ccfefd26ea1b35d2f1bf1a13a654298a0
dhruv8433 <dhruvrsoni04@gmail.com>
2023-09-23 17:02:12 +0530

    provider page

---
+++ app/[locale]/providers/Loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="h-screen items-center flex justify-center">Loading...</div>
+  );
+};
+
+export default Loading;
