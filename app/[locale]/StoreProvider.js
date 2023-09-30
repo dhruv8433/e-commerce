ec1525e9874b271d3c76da98435f4a6bf3e82641
dhruv8433 <dhruvrsoni04@gmail.com>
2023-09-30 22:41:04 +0530

    redux presist configuration

--- app/[locale]/StoreProvider.js
+++ app/[locale]/StoreProvider.js
@@ -1,9 +1,16 @@
 "use client";
 
 import { Provider } from "react-redux";
-import store from "./store";
+import { store, persistor } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 export const ProviderStore = ({ children }) => {
   // it support only on client component so that we create component and export it to layout
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 };
--- app/[locale]/store.js
+++ app/[locale]/store.js
@@ -1,10 +1,34 @@
-// import { composeWithDevTools } from "@redux-devtools/extension";
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
 import rootReducer from "../reducer/rootReducer";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
-const store = configureStore({
-  reducer: rootReducer,
+// Configure Redux Persist
+const persistConfig = {
+  key: "root", // The key to use for storing data
+  storage, // Choose between localStorage or sessionStorage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// for middle ware check https://redux-toolkit.js.org/usage/usage-guide#working-with-non-serializable-data
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-// Export the store for later use
-export default store;
+export const persistor = persistStore(store);
