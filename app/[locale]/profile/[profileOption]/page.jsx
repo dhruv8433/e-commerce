b1265436a817569749c22dadd4a8b5adbdcc6e35
dhruv8433 <dhruvrsoni04@gmail.com>
2023-10-02 22:01:02 +0530

    profile options

--- app/[locale]/profile/[profileOption]/page.jsx
+++ app/[locale]/profile/[profileOption]/page.jsx
@@ -1,13 +1,24 @@
 "use client";
 
+import ProfileBooking from "@/app/components/ProfileBooking";
 import UserProfile from "@/app/components/UserProfile";
+import { Container } from "@mui/material";
 import { useParams } from "next/navigation";
 import React from "react";
 
 const page = () => {
-  const router = useParams();
-  console.log(router.profileOption);
-  return <UserProfile content={router.profileOption} />;
+  const parmas = useParams();
+  console.log(parmas.profileOption);
+  return (
+    <Container>
+      {/* if params is bookings than render this section */}
+      {parmas && parmas.profileOption === "bookings" ? (
+        <UserProfile content={<ProfileBooking />} />
+      ) : (
+        <UserProfile />
+      )}
+    </Container>
+  );
 };
 
 export default page;
