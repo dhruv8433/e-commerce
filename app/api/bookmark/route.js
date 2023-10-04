d394c8e2e26e07e5765609a1d2f662f31476930a
dhruv8433 <dhruvrsoni04@gmail.com>
2023-10-04 15:22:32 +0530

    bookmark api

--- app/api/bookmark/route.js
+++ app/api/bookmark/route.js
@@ -1,10 +1,15 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
+  const items = await request.json();
+
+  let bookmarkItems = [];
+  bookmarkItems.push(items);
   return NextResponse.json(
     {
       success: true,
       message: "bookmark success",
+      data: bookmarkItems,
     },
     {
       status: 200,
