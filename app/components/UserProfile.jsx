afd8be9cbacd97175a0c5c855c9461f100fa7a08
dhruv8433 <dhruvrsoni04@gmail.com>
2023-10-15 20:00:28 +0530

    profile

--- app/components/UserProfile.jsx
+++ app/components/UserProfile.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import { LOGOUT_SUCCESS } from "../reducer/isLoggedIn";
 import toast from "react-hot-toast";
-import { Button } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import { useTranslations } from "next-intl";
 
 const UserProfile = ({ content }) => {
@@ -31,66 +31,76 @@ const UserProfile = ({ content }) => {
   const t = useTranslations("profile");
 
   return (
-    <div className="pt-28 w-full pb-8">
-      <div className="border border-red-300 bg-theme rounded">
-        <div className="flex">
-          <div className="user-image h-56 w-56 my-4 mx-4">
-            <Image
-              src={
-                "https://i.pinimg.com/736x/95/14/a2/9514a2106bdf506dc2ed2047ae4ba908.jpg"
-              }
-              width={"200"}
-              height={"200"}
-              alt="user-picture"
-              style={{
-                borderRadius: "120px",
-                maxHeight: "100%",
-                width: "100%",
-              }}
-            />
-          </div>
-          <div className="user-details flex items-center">
-            <div className="block">
-              <h1 className="text-3xl font-bold">{user.user}</h1>
-              <h1 className="text-2xl font-bold">{user.email}</h1>
-              <h1 className="text-2xl font-semibold">{user.mobile}</h1>
+    <div className="pt-28 w-full pb-8 text-center">
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={4}>
+          <div className="border border-red-300 bg-theme rounded justify-center">
+            <div>
+              <div className="user-image h-56 w-56 mx-auto text-center">
+                <Image
+                  src={
+                    "https://i.pinimg.com/736x/95/14/a2/9514a2106bdf506dc2ed2047ae4ba908.jpg"
+                  }
+                  width={"200"}
+                  height={"200"}
+                  alt="user-picture"
+                  style={{
+                    borderRadius: "120px",
+                    maxHeight: "100%",
+                    width: "100%",
+                  }}
+                />
+              </div>
+              <div className="user-details text-center items-center">
+                <div className="block">
+                  <h1 className="text-3xl font-bold">{user.user}</h1>
+                  <h1 className="text-2xl font-bold">{user.email}</h1>
+                  <h1 className="text-2xl font-semibold">{user.mobile}</h1>
+                </div>
+              </div>
+              <div className="edit ml-auto m-1">
+                <CustomButton
+                  children={"Edit"}
+                  varient={"outlined"}
+                  size={"small"}
+                  customClass={"text-white"}
+                />
+              </div>
             </div>
-          </div>
-          <div className="edit ml-auto m-1">
-            <CustomButton
-              children={"Edit"}
-              varient={"outlined"}
-              size={"small"}
-              customClass={"text-white"}
-            />
-          </div>
-        </div>
-        {/* user navigation links that contain common user profile but different user route */}
-        <div className="bg-white h-10 flex items-center">
-          <ul className="flex justify-around w-full">
-            <li>
-              <Link href={"/profile/bookings"}>{t("booking")}</Link>
-            </li>
-            <li>
-              <Link href={"/profile/bookmark"}>{t("bookmark")}</Link>
-            </li>
-            <li>
-              <Link href={"/profile/notifications"}>{t("notification")}</Link>
-            </li>
-            <li>
-              <Link href={"/profile/delete-account"}>
-                {t("delete_account")}
-              </Link>
-            </li>
-          </ul>
-        </div>
 
-        <div className="content">{content}</div>
-        
-        <Button varient={"contained"} onClick={() => logout()}>
-          Logout
-        </Button>
-      </div>
+            <Button varient={"contained"} onClick={() => logout()}>
+              Logout
+            </Button>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <div className="border border-red-300 rounded">
+            {/* user navigation links that contain common user profile but different user route */}
+            <div className="bg-white h-10 flex items-center">
+              <ul className="flex justify-around w-full">
+                <li>
+                  <Link href={"/profile/bookings"}>{t("booking")}</Link>
+                </li>
+                <li>
+                  <Link href={"/profile/bookmark"}>{t("bookmark")}</Link>
+                </li>
+                <li>
+                  <Link href={"/profile/notifications"}>
+                    {t("notification")}
+                  </Link>
+                </li>
+                <li>
+                  <Link href={"/profile/delete-account"}>
+                    {t("delete_account")}
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className="content">{content}</div>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 };
