9afbfce31f29d67efd046360a7ea2759e5b4234b
dhruv8433 <dhruvrsoni04@gmail.com>
2023-10-17 10:04:58 +0530

    custom button

--- app/common/CustomButton.jsx
+++ app/common/CustomButton.jsx
@@ -1,12 +1,23 @@
+/**
+ * This is reusable button that everywhere used in our project
+ */
+
 import { Button } from "@mui/material";
 import React from "react";
 
-const CustomButton = ({ children, size, varient, customClass }) => {
+const CustomButton = ({
+  children,
+  size,
+  varient,
+  customClass,
+  customFunction,
+}) => {
   return (
     <Button
       size={`${size}`}
       className={`${customClass} bg-violet-400 hover:bg-violet-500`}
       variant={`${varient}`}
+      onClick={customFunction}
     >
       {children}
     </Button>
