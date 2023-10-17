465b702ecebfce64a53167b02bde830c43222cad
dhruv8433 <dhruvrsoni04@gmail.com>
2023-10-17 11:12:26 +0530

    reduce to add items inside cart

---
+++ app/reducer/cartReducer.js
@@ -0,0 +1,35 @@
+// initial stage for cart functionallity
+const initialState = {
+  data: [],
+  status: "pending",
+};
+
+// Define action types (constants)
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_TO_CART";
+
+// reducer that handle which case fired
+const cartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        data: [...state.data, action.payload],
+        status: "pending",
+      };
+    case REMOVE_FROM_CART:
+      // action.payload should contain the item to be removed from the cart
+      const updatedCart = state.cart.filter(
+        (item) => item.id !== action.payload.id
+      );
+      return {
+        ...state,
+        cart: updatedCart,
+        status: "pending",
+      };
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
--- app/reducer/rootReducer.js
+++ app/reducer/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import counterReducer from "./counterReducer";
 import authReducer from "./isLoggedIn";
+import cartReducer from "./cartReducer";
 
 const rootReducer = combineReducers({
   counter: counterReducer,
   isAuthenticate: authReducer,
+  cart: cartReducer,
 });
 
 export default rootReducer;
