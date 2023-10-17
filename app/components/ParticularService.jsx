9592cdf2e3a7ee9c92de982ed9b7632af3c09dab
dhruv8433 <dhruvrsoni04@gmail.com>
2023-10-17 21:26:59 +0530

    service cart

--- app/components/ParticularService.jsx
+++ app/components/ParticularService.jsx
@@ -1,4 +1,4 @@
-import { Button, Container, Grid } from "@mui/material";
+import { Box, Button, Container, Grid } from "@mui/material";
 import React from "react";
 import toast from "react-hot-toast";
 import CustomButton from "../common/CustomButton";
@@ -39,7 +39,13 @@ const ParticularService = ({ service }) => {
       <div className="h-auto my-10">
         <Grid container spacing={4}>
           <Grid item xs={12} md={6}>
-            <div className="image" style={{ height: "600px", width: "500px" }}>
+            <Box
+              className="image"
+              sx={{
+                height: { xs: "300px", md: "460px" },
+                width: { xs: "360px", md: "500px" },
+              }}
+            >
               <img
                 src={service.image}
                 style={{
@@ -50,38 +56,40 @@ const ParticularService = ({ service }) => {
                 }}
                 alt={service.title}
               />
-            </div>
+            </Box>
           </Grid>
           <Grid item xs={12} md={6}>
-            <div className="info p-4 border border-gray-300 rounded-lg">
-              <h1 className="text-gray-600 text-2xl">{service.title}</h1>
-              <h1 className="color-primary text-4xl">
-                ${service.discounted_price}
-                <span className="text-lg text-gray-500">
-                  <del>{service.price}</del>
-                </span>
-              </h1>
-              <h1 className="p-1 bg-green-400 w-fit px-3 rounded">
-                {service.rating}
-              </h1>
-            </div>
+            <Box sx={{ mt: { xs: 5, md: 0 } }}>
+              <div className="info p-4 border border-gray-300 rounded-lg">
+                <h1 className="text-gray-600 text-2xl">{service.title}</h1>
+                <h1 className="color-primary text-4xl">
+                  ${service.discounted_price}
+                  <span className="text-lg text-gray-500">
+                    <del>{service.price}</del>
+                  </span>
+                </h1>
+                <h1 className="p-1 bg-green-400 w-fit px-3 rounded">
+                  {service.rating}
+                </h1>
+              </div>
 
-            <div className="info mt-4 p-4 border border-gray-300 rounded-lg">
-              <h1 className="text-2xl font-semibold">Service Description:</h1>
-              <h1 className="mt-4">{service.description}</h1>
-            </div>
+              <div className="info mt-4 p-4 border border-gray-300 rounded-lg">
+                <h1 className="text-2xl font-semibold">Service Description:</h1>
+                <h1 className="mt-4">{service.description}</h1>
+              </div>
 
-            <CustomButton
-              children={"Add to Cart"}
-              varient={"contained"}
-              customClass={"w-full my-10"}
-              customFunction={() => addItemsToCart()}
-            >
-              Add to card
-            </CustomButton>
-            <Button fullWidth sx={{ mt: 1, mb: 1 }} variant="outlined">
-              Add to Wishlist
-            </Button>
+              <CustomButton
+                children={"Add to Cart"}
+                varient={"contained"}
+                customClass={"w-full my-10"}
+                customFunction={() => addItemsToCart()}
+              >
+                Add to card
+              </CustomButton>
+              <Button fullWidth sx={{ mt: 1, mb: 1 }} variant="outlined">
+                Add to Wishlist
+              </Button>
+            </Box>
           </Grid>
         </Grid>
       </div>
--- app/components/ServiceCard.jsx
+++ app/components/ServiceCard.jsx
@@ -1,57 +1,70 @@
-import React, { useEffect } from "react";
-import { Card, CardContent, CardMedia } from "@mui/material";
-import CustomButton from "../common/CustomButton";
+import React from "react";
+import { Card, CardContent, CardMedia, IconButton } from "@mui/material";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
 import { useParams } from "next/navigation";
+import { DeleteOutline } from "@mui/icons-material";
+import toast from "react-hot-toast";
 
-const ServiceCard = ({ services }) => {
+const ServiceCard = ({ services, deleteIcon }) => {
   const t = useTranslations("service");
   // to navigate particular service page
   const { providerId } = useParams();
   const { providerSlug } = useParams();
 
-  return (
-    <Link
-      href={`/${providerId}/${providerSlug}/${services.id}/${services.slug}`}
-    >
-      <Card className="my-3 hover:cursor-pointer" sx={{ p: 1, height: 320 }}>
-        {/* Left side with image, title, and rating */}
-        <CardMedia
-          component="img"
-          sx={{ width: "100%", height: 200, borderRadius: "10px" }}
-          image={services.image}
-          alt=""
-        />
-        <CardContent>
-          <h1 className="font-semibold text-1xl">{services.title}</h1>
-          <p className="text-slate-500">
-            {t("rating")}:{" "}
-            <span className="bg-green-500 px-2 rounded">{services.rating}</span>
-          </p>
+  const deleteService = () => {
+    toast.success("Service Deleted Success !");
+  };
 
-          {/* Right side with the "Add" button */}
-          <div className="mt-2 flex justify-between">
-            <div className="price">
-              <p className="text-blue-500 text-2xl">
-                ${services.discounted_price}
-                <del>
-                  <span className="text-xs text-slate-500">
-                    {services.price}
-                  </span>
-                </del>
-              </p>
+  return (
+    <>
+      <Link
+        href={`/${providerId}/${providerSlug}/${services.id}/${services.slug}`}
+      >
+        <Card className="my-3 hover:cursor-pointer" sx={{ p: 1, height: 320 }}>
+          {/* Left side with image, title, and rating */}
+          <CardMedia
+            component="img"
+            sx={{ width: "100%", height: 200, borderRadius: "10px" }}
+            image={services.image}
+            alt=""
+          />
+          <CardContent>
+            <h1 className="font-semibold text-1xl">{services.title}</h1>
+            <p className="text-slate-500">
+              {t("rating")}:{" "}
+              <span className="bg-green-500 px-2 rounded">
+                {services.rating}
+              </span>
+            </p>
+
+            {/* Right side with the "Add" button */}
+            <div className="mt-2 flex justify-between items-center">
+              <div className="price">
+                <p className="text-blue-500 text-2xl">
+                  ${services.discounted_price}
+                  <del>
+                    <span className="text-xs text-slate-500">
+                      {services.price}
+                    </span>
+                  </del>
+                </p>
+              </div>
             </div>
-            {/* <CustomButton
-              children={t("add")}
-              size={"small"}
-              customClass={"bg-blue-500"}
-              variant={"contained"} // Corrected the prop name
-            /> */}
-          </div>
-        </CardContent>
-      </Card>
-    </Link>
+          </CardContent>
+        </Card>
+      </Link>
+      {deleteIcon === true ? (
+        <IconButton
+          sx={{ mt: "-100px", ml: "80%" }}
+          onClick={() => deleteService()}
+        >
+          <DeleteOutline sx={{ color: "red" }} />
+        </IconButton>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
