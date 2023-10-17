a568367d4e4e2f082838e78e3791f4abccdfe571
dhruv8433 <dhruvrsoni04@gmail.com>
2023-10-17 10:04:34 +0530

    detailed service

---
+++ app/[locale]/[providerId]/[providerSlug]/[serviceId]/[serviceSlug]/page.jsx
@@ -0,0 +1,37 @@
+"use client";
+import ParticularService from "@/app/components/ParticularService";
+import { getService } from "@/app/services/getServices";
+import { Box } from "@mui/material";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const page = () => {
+  // first we want provider id to find which provider service is this and based on that we find that particular service on that provider
+  const { serviceId } = useParams();
+  const { providerId } = useParams();
+
+  // service data set
+  const [service, setService] = useState([]);
+
+  // async function to find particular provider
+  async function particularProvider() {
+    const response = await getService(providerId);
+    setService(response.data);
+  }
+
+  useEffect(() => {
+    particularProvider();
+  }, []);
+  return (
+    <Box sx={{ pt: { xs: "4rem", md: "5rem" } }}>
+      {service.map((response) => {
+        //  if service id existing inside provider service
+        if (serviceId == response.id)
+          // if condition match then return this component
+          return <ParticularService key={response.id} service={response} />;
+      })}
+    </Box>
+  );
+};
+
+export default page;
