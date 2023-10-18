a26a956d07261c109456e1b4fa7c37348f829aa6
dhruv8433 <dhruvrsoni04@gmail.com>
2023-10-18 21:07:56 +0530

    middleware

--- middleware.js
+++ middleware.js
@@ -1,12 +1,33 @@
 import createMiddleware from "next-intl/middleware";
+import { NextResponse } from "next/server";
 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ["en", "hi", "fr"],
+const headersOption = () => {
+  const res = NextResponse.next();
+  // add the CORS headers to the response
+  res.headers.append("Access-Control-Allow-Credentials", "true");
+  res.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
+  res.headers.append(
+    "Access-Control-Allow-Methods",
+    "GET,DELETE,PATCH,POST,PUT"
+  );
+  res.headers.append(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
 
-  // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-  defaultLocale: "en",
-});
+  return res;
+};
+
+export default createMiddleware(
+  {
+    // A list of all locales that are supported
+    locales: ["en", "hi", "fr"],
+
+    // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
+    defaultLocale: "en",
+  },
+  headersOption
+);
 
 export const config = {
   // Skip all paths that should not be internationalized. This example skips the
