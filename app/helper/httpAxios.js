a89bdd4ca4e2dac21842a6edbd4d322246b168ab
dhruv8433 <dhruvrsoni04@gmail.com>
2023-10-18 19:20:29 +0530

    url

--- app/helper/httpAxios.js
+++ app/helper/httpAxios.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 // here we create a httpaxios for our website base url
 export const httpAxios = axios.create({
-  baseURL:
-    "http://localhost:8000/" &&
-    "https://e-requirement-odudy69yq-dhruv8433.vercel.app/",
+  // production live url
+  baseURL: "https://e-requirement-odudy69yq-dhruv8433.vercel.app/",
 });
+// // here we create a httpaxios for our website base url
+// export const httpAxios = axios.create({
+// //  localhost live
+//   baseURL: "http://localhost:8000",
+// });
