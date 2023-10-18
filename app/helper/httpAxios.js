625bdac763e54803fae52b8c657c28137be454db
dhruv8433 <dhruvrsoni04@gmail.com>
2023-10-18 19:58:30 +0530

    axios configuration

--- app/helper/httpAxios.js
+++ app/helper/httpAxios.js
@@ -1,15 +1,11 @@
-import { crossOrigin } from "@/next.config";
 import axios from "axios";
 
-crossOrigin: "*"
-
+// export const httpAxios = axios.create({
+//   // production live url
+//   baseURL: "https://e-requirement-odudy69yq-dhruv8433.vercel.app",
+// });
 // here we create a httpaxios for our website base url
 export const httpAxios = axios.create({
-  // production live url
-  baseURL: "https://e-requirement-odudy69yq-dhruv8433.vercel.app",
+  //  localhost live
+  baseURL: "http://localhost:8000",
 });
-// // here we create a httpaxios for our website base url
-// export const httpAxios = axios.create({
-// //  localhost live
-//   baseURL: "http://localhost:8000",
-// });
