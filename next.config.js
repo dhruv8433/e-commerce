608acfa82eed200bfc491e422f6f2509119f5979
dhruv8433 <dhruvrsoni04@gmail.com>
2023-10-18 20:09:32 +0530

    configurations

--- next.config.js
+++ next.config.js
@@ -1,18 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["i.pinimg.com", "media.istockphoto.com"],
-  },
-  // allow cors origin policys to access website while in production development
   async headers() {
     return [
       {
+        // matching all API routes
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
         ],
       },
     ];
+  },
+  images: {
+    domains: ["i.pinimg.com", "media.istockphoto.com"],
   },
 };
 
