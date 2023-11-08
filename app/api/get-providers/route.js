a68c372703b548cb9210fa8a3a9eedeecf4888a4
dhruv8433 <dhruvrsoni04@gmail.com>
2023-11-08 09:26:54 +0530

    getting data from db

--- app/api/get-providers/route.js
+++ app/api/get-providers/route.js
@@ -1,33 +1,17 @@
+import { ProviderModel } from "@/app/model/providerModel";
 import { NextResponse } from "next/server";
-import homeProvider from "./json/home-provider.json";
-import laundryProvider from "./json/laundry-provider.json";
-import fullProvider from "./json/full-provider.json";
-import barberProvider from "./json/barber-provider.json";
-import electricProvider from "./json/electric-provider.json";
-import pestControlProvider from "./json/pest-control-provider.json";
-import plumbingProvider from "./json/plumbing-provider.json";
-import kitchenProvider from "./json/kitchen-provider.json";
-import carProvider from "./json/car-provider.json";
 
+// fetching all providerss
 export async function POST(request) {
-  return NextResponse.json(
-    {
-      success: true,
-      message: `Providers Fetchign Success`,
-      data: [
-        homeProvider,
-        laundryProvider,
-        fullProvider,
-        barberProvider,
-        electricProvider,
-        pestControlProvider,
-        plumbingProvider,
-        kitchenProvider,
-        carProvider,
-      ],
-    },
-    {
-      status: 200,
-    }
-  );
+  let providers = [];
+  try {
+    providers = await ProviderModel.find();
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({
+      success: false,
+      message: `unable to fetching Providers`,
+    });
+  }
+  return NextResponse.json(providers);
 }
