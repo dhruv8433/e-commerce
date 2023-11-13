a19d7911045c7d4160466e9749950e85dc700b6f
dhruv8433 <dhruvrsoni04@gmail.com>
2023-11-13 22:33:07 +0530

    profile option

--- app/[locale]/profile/[profileOption]/page.jsx
+++ app/[locale]/profile/[profileOption]/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Bookings from "@/app/components/Bookings";
+import Notification from "@/app/components/Notification";
 import UserProfile from "@/app/components/UserProfile";
 import { Container } from "@mui/material";
 import { useParams } from "next/navigation";
@@ -9,18 +10,26 @@ import React from "react";
 const page = () => {
   const parmas = useParams();
   console.log(parmas.profileOption);
-  return (
-    <Container>
-      {/* if params is bookings than render this section */}
-      {parmas && parmas.profileOption === "bookings" ? (
-        <>
-          <UserProfile data={<Bookings />} changeRoute={true} />
-        </>
-      ) : (
+
+  if (parmas && parmas.profileOption == "bookings") {
+    return (
+      <Container>
+        <UserProfile data={<Bookings />} changeRoute={true} />
+      </Container>
+    );
+  } else if (parmas && parmas.profileOption == "notifications") {
+    return (
+      <Container>
+        <UserProfile data={<Notification />} changeRoute={true} />
+      </Container>
+    );
+  } else {
+    return (
+      <Container>
         <UserProfile />
-      )}
-    </Container>
-  );
+      </Container>
+    );
+  }
 };
 
 export default page;
