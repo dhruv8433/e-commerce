ad06a9f362bf95cb9250d0daadfbf2be53511b15
dhruv8433 <dhruvrsoni04@gmail.com>
2023-11-14 12:23:47 +0530

    booking

--- app/components/Bookings.jsx
+++ app/components/Bookings.jsx
@@ -3,15 +3,18 @@ import React, { useEffect, useState } from "react";
 import { GetOrdersService } from "../services/getOrdersService";
 import { useSelector } from "react-redux";
 import SummeryCard from "./SummeryCard";
+import { BookingSkeleton } from "./Skeletons";
 
 const Bookings = () => {
   const [services, setServices] = useState([]);
+  const [loading, setLoading] = useState(true);
   const token = useSelector((state) => state.isAuthenticate.user.token);
   async function gettingServices() {
     try {
       const service = await GetOrdersService(token);
       console.log(service);
       setServices(service);
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -23,12 +26,20 @@ const Bookings = () => {
     <>
       <h1 className="text-3xl font-bold">Bookings</h1>
       <hr />
-      {services.map((order) => {
-        if (order.userToken === token) {
-          let total = 0;
-          return <SummeryCard key={order._id} order={order} total={total} />;
-        }
-      })}
+      {loading ? (
+        <>
+          <BookingSkeleton />
+          <BookingSkeleton />
+          <BookingSkeleton />
+        </>
+      ) : (
+        services.map((order) => {
+          if (order.userToken === token) {
+            let total = 0;
+            return <SummeryCard key={order._id} order={order} total={total} />;
+          }
+        })
+      )}
     </>
   );
 };
