c6b47a185c6eb006b3cbe7c42a0ae7b7fe121a99
dhruv8433 <dhruvrsoni04@gmail.com>
2023-11-14 17:54:26 +0530

    provider service page

--- app/[locale]/[providerId]/[providerSlug]/page.jsx
+++ app/[locale]/[providerId]/[providerSlug]/page.jsx
@@ -1,14 +1,12 @@
 "use client";
 
-import ServiceCard from "@/app/components/ServiceCard";
 import { getService } from "@/app/services/getServices";
 import { GetSingleProvider } from "@/app/services/getProviders";
 import { Container, Grid } from "@mui/material";
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
-import DetailedProvider from "@/app/components/DetailedProvider";
-import { company_name } from "@/app/config/config";
-import ProviderSkeleton, { Serviceskeleton } from "@/app/components/Skeletons";
+import ProviderDetailsPage from "@/app/components/ProviderDetailsPage";
+import Services from "@/app/components/Services";
 
 const page = () => {
   const [services, setServices] = useState([]);
@@ -35,7 +33,6 @@ const page = () => {
   }
 
   useEffect(() => {
-    // temp delay and without that we can't see page
     getServices();
   }, []);
 
@@ -44,43 +41,17 @@ const page = () => {
       <div className="breadcrums"></div>
       <Container>
         <Grid container spacing={2}>
-          <Grid item sm={12} md={4}>
-            <div className="right mt-24">
-              {providerLoading ? (
-                <ProviderSkeleton />
-              ) : (
-                provider.map((singleProvider) => {
-                  document.title = `${singleProvider.title} | ${company_name}`;
-                  return <DetailedProvider provider={singleProvider} />;
-                })
-              )}
-            </div>
-            <div className="mt-4  rounded-md justify-center items-center bg-slate-100">
-              <div className="flex justify-center">
-                <h1 className="text-2xl py-1">User Reviews</h1>
-              </div>
-            </div>
-          </Grid>
-          <Grid item sm={12} md={8}>
-            <div className="left mt-24 p-3 rounded bg-slate-100">
-              <Grid container spacing={3}>
-                {serviceLoading ? (
-                  <SkeletonServiceLoading />
-                ) : (
-                  services.map((service) => {
-                    // if service's provider id match with this provider id that we got from params
-                    if (service.provider_id == providerId) {
-                      return (
-                        <Grid item xs={12} sm={8} md={4} key={service.id}>
-                          <ServiceCard key={service.id} services={service} />
-                        </Grid>
-                      );
-                    }
-                  })
-                )}
-              </Grid>
-            </div>
-          </Grid>
+          {/* particular provider component */}
+          <ProviderDetailsPage
+            providerLoading={providerLoading}
+            provider={provider}
+          />
+          {/* related provider services */}
+          <Services
+            serviceLoading={serviceLoading}
+            services={services}
+            providerId={providerId}
+          />
         </Grid>
       </Container>
     </div>
@@ -88,27 +59,3 @@ const page = () => {
 };
 
 export default page;
-
-const SkeletonServiceLoading = () => {
-  return (
-    <>
-      <Grid container spacing={2} paddingY={1}>
-        <Grid item xs={12} sm={8} md={4}>
-          <Serviceskeleton />
-          <Serviceskeleton />
-          <Serviceskeleton />
-        </Grid>
-        <Grid item xs={12} sm={8} md={4}>
-          <Serviceskeleton />
-          <Serviceskeleton />
-          <Serviceskeleton />
-        </Grid>
-        <Grid item xs={12} sm={8} md={4}>
-          <Serviceskeleton />
-          <Serviceskeleton />
-          <Serviceskeleton />
-        </Grid>
-      </Grid>
-    </>
-  );
-};
