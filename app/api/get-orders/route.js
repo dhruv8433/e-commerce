67de925d8229fc34a3b0bef15e06912d8c7bc564
dhruv8433 <dhruvrsoni04@gmail.com>
2023-11-14 09:43:48 +0530

    user orders

--- app/api/get-orders/route.js
+++ app/api/get-orders/route.js
@@ -5,8 +5,10 @@ import { NextResponse } from "next/server";
 dbConnect();
 
 export async function POST(request) {
+  const { userToken } = await request.json();
+  let orders = [];
   try {
-    const orders = await placeOrder.find();
+    orders = await placeOrder.find({ userToken });
     return NextResponse.json(orders);
   } catch (error) {
     console.log(error);
