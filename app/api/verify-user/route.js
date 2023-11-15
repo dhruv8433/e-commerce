7a623ba4f0e35a719faee4feb767797b8e83eb90
dhruv8433 <dhruvrsoni04@gmail.com>
2023-11-15 13:09:59 +0530

    verify user

--- app/api/verify-user/route.js
+++ app/api/verify-user/route.js
@@ -9,19 +9,6 @@ export async function POST(request) {
   const { email, password } = await request.json();
 
   try {
-    // guest user for testing purpos
-    if (email === "guestuser@gmail.com" && password === "12345") {
-      return NextResponse.json({
-        success: true,
-        message: "Guest user login success",
-        token: "cjbtrbyrpgybhnvjHIBIijbhVUovhuVHUOhuvhuvVHUGVGvvUHVgvGGVBhOGK",
-        user: "Guest User",
-        email: "guestuser@gmail.com",
-        mobile: 1234567890,
-        address: "abc area, abc city",
-      });
-    }
-
     // Find the user by email
     const user = await userModel.findOne({ email });
 
@@ -36,9 +23,9 @@ export async function POST(request) {
           success: true,
           message: "User Login Success",
           token: user.token,
-          user: user.name,
+          name: user.name,
           email: user.email,
-          mobile: user.phone,
+          phone: user.phone,
           address: user.address,
         },
         {
