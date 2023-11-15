1c2d2e93d03b29ec90b234988312a235c150e337
dhruv8433 <dhruvrsoni04@gmail.com>
2023-11-15 09:04:54 +0530

    provider

--- app/components/Provider.jsx
+++ app/components/Provider.jsx
@@ -1,18 +1,52 @@
 "use client";
 
 import { EastRounded, StarBorderOutlined } from "@mui/icons-material";
-import { Card, CardContent, CardMedia } from "@mui/material";
+import { Box, Card, CardContent, CardMedia, Checkbox } from "@mui/material";
 import waveAnimation from "@/app/json/animations/wave.json";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
 import Lottie from "lottie-react";
 import Link from "next/link";
 import React from "react";
 import { useTranslations } from "next-intl";
+import { BookmarkService } from "../services/bookmarkService";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
 
 // reusable provider card
-const Provider = ({ provider }) => {
+const Provider = ({ provider, bookmarkIconVisible }) => {
   const t = useTranslations("providers");
+  const token = useSelector((state) => state.isAuthenticate.user.token);
+
+  async function toogleBookmark(data) {
+    const response = await BookmarkService(data, token);
+    console.log(response);
+    // Check if the bookmark was added or deleted
+    if (response.message === "Bookmark added successfully") {
+      toast.success("Added to bookmarks");
+    } else if (response.message === "Bookmark deleted successfully") {
+      toast.success("Removed from bookmarks");
+    }
+  }
   return (
     <div className="p-1 items-center text-center">
+      {bookmarkIconVisible ? (
+        <Box
+          className="absolute rounded-sm bg-white"
+          sx={{
+            ml: 41,
+          }}
+        >
+          <Checkbox
+            icon={<BookmarkBorderIcon />}
+            checkedIcon={<BookmarkIcon />}
+            onClick={() => toogleBookmark(provider)}
+          />
+        </Box>
+      ) : (
+        ""
+      )}
+
       <Link href={`/${provider.id}/${provider.slug}`}>
         <Card>
           <CardMedia
