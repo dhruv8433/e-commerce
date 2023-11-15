f0ab3c5c30a3043b164a7ad9a5a4d54d54125b96
dhruv8433 <dhruvrsoni04@gmail.com>
2023-11-15 09:04:24 +0530

    bookmark

--- app/api/bookmark/route.js
+++ app/api/bookmark/route.js
@@ -1,22 +1,42 @@
 import { dbConnect } from "@/app/database/db";
+import { BookmarkModel } from "@/app/model/bookmarkModel";
 import { NextResponse } from "next/server";
 
 dbConnect();
 
+// toggle bookmark
 export async function POST(request) {
-  const items = await request.json();
+  const { providerData, token } = await request.json();
 
-  let bookmarkItems = [];
-  //we have to create add, delete and also get book mark api
-  bookmarkItems.push(items);
-  return NextResponse.json(
-    {
-      success: true,
-      message: "bookmark success",
-      data: bookmarkItems,
-    },
-    {
-      status: 200,
+  try {
+    // Check if the bookmark already exists for the given token and providerData
+    const existingBookmark = await BookmarkModel.findOne({
+      token,
+      data: providerData,
+    });
+
+    if (existingBookmark) {
+      // Bookmark exists, delete it
+      await BookmarkModel.deleteOne({ token: token, data: providerData });
+      return NextResponse.json({
+        success: true,
+        message: "Bookmark deleted successfully",
+      });
+    } else {
+      // Bookmark doesn't exist, add it
+      const bookmark = new BookmarkModel({ data: providerData, token });
+      const bookmarkedData = await bookmark.save();
+      return NextResponse.json({
+        success: true,
+        message: "Bookmark added successfully",
+        data: bookmarkedData,
+      });
     }
-  );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({
+      success: false,
+      error: error,
+    });
+  }
 }
