254c31fdf36b28e96a993881956d33f4dc338e24
dhruv8433 <dhruvrsoni04@gmail.com>
2023-11-16 17:57:55 +0530

    cart drawer

--- app/drawers/CartDrawer.jsx
+++ app/drawers/CartDrawer.jsx
@@ -8,12 +8,15 @@ import ServiceCard from "../components/ServiceCard";
 import emptyCartAnimation from "@/app/json/animations/empty_cart.json";
 import Lottie from "lottie-react";
 import StripeCheckoutButton from "../components/CheckOut";
+import { useTranslations } from "next-intl";
 
 const CartDrawer = () => {
   const [openCart, setOpenCart] = useState(false);
 
   const items = useSelector((state) => state.cart.cartItems);
   let price = 0;
+
+  const t = useTranslations("drawer");
 
   return (
     <>
@@ -23,7 +26,7 @@ const CartDrawer = () => {
       <Drawer anchor="right" open={openCart} onClose={() => close(setOpenCart)}>
         <Box sx={{ width: { xs: "300px", md: "400px" } }}>
           <div className="p-1">
-            <h1 className="font-semibold text-2xl">Your Services</h1>
+            <h1 className="font-semibold text-2xl">{t("service")}</h1>
             <Divider />
             {/* reuser service card and i have to just display delete icon on that based on props -- simple */}
             {items.length > 0 ? (
