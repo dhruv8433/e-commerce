f5775784fed4085461621de78a6e8b12f11b54f5
dhruv8433 <dhruvrsoni04@gmail.com>
2023-11-16 17:58:12 +0530

    service detailed

--- app/[locale]/[providerId]/[providerSlug]/[serviceId]/[serviceSlug]/page.jsx
+++ app/[locale]/[providerId]/[providerSlug]/[serviceId]/[serviceSlug]/page.jsx
@@ -1,8 +1,10 @@
 "use client";
 import ParticularService from "@/app/components/ParticularService";
+import { ServiceDetailsSkeleton, Serviceskeleton } from "@/app/components/Skeletons";
 import { company_name } from "@/app/config/config";
 import { getService } from "@/app/services/getServices";
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
+import { useTranslations } from "next-intl";
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
@@ -13,27 +15,37 @@ const page = () => {
 
   // service data set
   const [service, setService] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // async function to find particular provider
   async function particularProvider() {
     const response = await getService(providerId);
     setService(response);
+    setLoading(false);
   }
 
   useEffect(() => {
     particularProvider();
   }, []);
+
+  const t = useTranslations("service")
   return (
     <Box sx={{ pt: { xs: "4rem", md: "5rem" } }}>
-      {service.map((response) => {
-        //  if service id existing inside provider service
-        if (serviceId == response.id) {
-          // title based on service
-          document.title = `${response.title} | ${company_name}`;
-          // if condition match then return this component
-          return <ParticularService key={response.id} service={response} />;
-        }
-      })}
+      {loading ? (
+        <Container>
+          <ServiceDetailsSkeleton />
+        </Container>
+      ) : (
+        service.map((response) => {
+          //  if service id existing inside provider service
+          if (serviceId == response.id) {
+            // title based on service
+            document.title = `${response.title} | ${company_name}`;
+            // if condition match then return this component
+            return <ParticularService key={response.id} service={response} t={t} />;
+          }
+        })
+      )}
     </Box>
   );
 };
