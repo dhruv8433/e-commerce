7b7d3edab7a05e6897844b04c55f551a1585d412
dhruv8433 <dhruvrsoni04@gmail.com>
2023-12-16 19:20:23 +0530

    final modifications

--- app/components/HomeAnimation.jsx
+++ app/components/HomeAnimation.jsx
@@ -4,7 +4,7 @@ import Lottie from "lottie-react";
 import React from "react";
 import homeAnimationJSON from "@/app/json/animations/homescreen.json";
 import { useTranslations } from "next-intl";
-import { Grid } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import "@/app/styles/style.css";
 
 // starting component
@@ -14,23 +14,30 @@ const HomeAnimation = () => {
   return (
     <>
       <div className="h-full">
-        <Grid container>
-          <Grid item xs={12} md={6}>
-            <div>
-              {/* some animation for home screen */}
-              <Lottie animationData={homeAnimationJSON} />
-            </div>
+        <Container>
+          <Grid container>
+            <Grid item xs={12} md={6}>
+              <div>
+                {/* some animation for home screen */}
+                <Lottie animationData={homeAnimationJSON} />
+              </div>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              className="flex justify-start items-center"
+            >
+              <div className="p-8 items-center">
+                <h1 className="text-4xl font-bold mb-4 text-gray-800">
+                  {t("welcome")}
+                </h1>
+                <p className="text-lg text-gray-600">{t("one_stop")}</p>
+                <p className="text-sm text-gray-400">{t("explore_here")}</p>
+              </div>
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={6} className="flex justify-start items-center">
-            <div className="p-8 items-center">
-              <h1 className="text-4xl font-bold mb-4 text-gray-800">
-                {t("welcome")}
-              </h1>
-              <p className="text-lg text-gray-600">{t("one_stop")}</p>
-              <p className="text-sm text-gray-400">{t("explore_here")}</p>
-            </div>
-          </Grid>
-        </Grid>
+        </Container>
       </div>
     </>
   );
