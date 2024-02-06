4d716f64e4696d97d087340c10aa6a77c969ae30
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-06 08:16:40 +0530

    web analytics and speed Insights

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -7,7 +7,9 @@ import Navigation from "../components/NavBar";
 import Footer from "../components/Footer";
 import { ProviderStore } from "./StoreProvider";
 import { setLocale } from "../config/config";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+// web analytics and speed Insights
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 require("dotenv").config();
 
 // static params for our languages file
@@ -20,13 +22,12 @@ export default async function RootLayout({ children, params }) {
   const { locale } = params;
   // when user want to change localets
   try {
-    // if 
+    // if
     setLocale();
     messages = (await import(`../../locales/${locale}.json`)).default;
   } catch (error) {
     notFound();
   }
-
 
   return (
     // passing locale as lang in html so we can translate in defined json language file
@@ -46,10 +47,11 @@ export default async function RootLayout({ children, params }) {
           <ProviderStore>
             <Navigation />
             {children}
+            <SpeedInsights />
+            <Analytics />
             <Footer />
             <Toaster />
           </ProviderStore>
-          <SpeedInsights />
         </NextIntlClientProvider>
       </body>
     </html>
